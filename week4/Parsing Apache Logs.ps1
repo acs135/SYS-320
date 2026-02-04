a40bd971be2cff7fa4fae0148a70261ebf43d6f1
unknown <acs135@yahoo.com>
2026-02-04 15:02:39 -0500

    parsing apache logs

---
+++ week4/Parsing Apache Logs.ps1
@@ -0,0 +1,31 @@
+﻿cd $PSScriptRoot
+
+function GetApacheLogs {
+    $accesslogtable = @()
+
+    Get-Content C:\xampp\apache\logs\access.log | ForEach-Object {
+        $log = ($_ -split " ")
+        $ip = $log[0]
+        $time = $log[3]+$log[4]
+        $method = $log[5].Trim('"')
+        $page = $log[6]
+        $protocol = $log[7].Trim('"')
+        $response = $log[8]
+        $responsesize = $log[9]
+        $referer = $log[10]
+        $useragent = ($log[11..($log.Length-1)] -join ' ').Trim('"')
+
+        $accesslogtable += [PSCustomObject]@{
+            "IP" = $ip
+            "Time" = $time
+            "Method" = $method
+            "Page" = $page
+            "Protocol" = $protocol
+            "Response" = $response
+            "Response Size" = $responsesize
+            "Referer" = $referer
+            "User Agent" = $useragent
+        }
+    }
+    $accesslogtable
+}
---
+++ week4/main.ps1
@@ -0,0 +1,7 @@
+﻿cd $PSScriptRoot
+
+. (Join-Path $PSScriptRoot '.\Parsing Apache Logs.ps1')
+
+GetApacheLogs | Format-Table
+
+GetApacheLogs | Where-Object { $_.Page -ilike "*/page2.html*" -and $_.Referer -ilike "*index.html*"} | Format-Table
