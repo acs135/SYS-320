a22acf296375e237c9986e174d49a5d4737ff8e4
unknown <acs135@yahoo.com>
2026-02-25 14:07:38 -0500

    week 7

---
+++ week7/Email.ps1
@@ -0,0 +1,11 @@
+﻿function SendAlertEmail($body){
+    $from = "aidan.stewart@mymail.champlain.edu"
+    $to = "aidan.stewart@mymail.champlain.edu"
+    $subject = "Suspicious Activity"
+
+    $password = Get-Content "file.txt" | ConvertTo-SecureString
+    $credential = New-Object -TypeName System.Management.Automation.PSCredential -ArgumentList $from, $password
+
+    Send-MailMessage -From $from -To $to -Subject $subject -Body $body -SmtpServer "smtp.gmail.com" -Port 587 -UseSsl -Credential $credential
+
+}
---
+++ week7/main.ps1
@@ -0,0 +1,12 @@
+﻿. (Join-Path $PSScriptRoot "Configuration.ps1")
+. (Join-Path $PSScriptRoot "Email.ps1")
+. (Join-Path $PSScriptRoot "Scheduler.ps1")
+. (Join-Path $PSScriptRoot "../week6/Event-Logs.ps1")
+
+$configuration = readConfiguration
+
+$failed = getFailedLogins $configuration.Days | Group-Object -Property User | Where-Object {$_.Count -gt 10} | Select-Object Name, Count
+
+SendAlertEmail ($failed | Format-Table | Out-String)
+
+ChooseTimeToRun ($configuration.ExecutionTime)
